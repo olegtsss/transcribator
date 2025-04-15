bd2dc23bf34088d3c50d050f6638fdc873f247a4
otymoshchuk <otymoshchuk@radiofid.ru>
2025-04-15 16:18:24 +0300

    consumer: fix auto_delete

--- consumer/main.py
+++ consumer/main.py
@@ -24,40 +24,39 @@ class Worker:
 
     @error_handling
     async def process_message(self, message: aio_pika.abc.AbstractIncomingMessage) -> None:
-        async with self.lock:
-            async with message.process():
-                try:
-                    data = LoadData.model_validate_json(message.body.decode())
-                except ValidationError:
-                    logger.error(Messanges.VALIDATION_ERROR.value, message.body.decode())
-                    return
-                if not os.access(data.audio_path, os.R_OK):
-                    logger.error(Messanges.FILE_NOT_EXIST.value, data.audio_path)
-                    return
-                with open(data.audio_path, mode='rb') as file:
-                    transcript = self.openai_client.audio.transcriptions.create(
-                        model=settings.openai_model, file=file
-                    )
-                if transcript.text:
-                    logger.info(
-                        Messanges.TRANSCRIPT_SUCCESS.value, data.audio_path, len(transcript.text)
-                    )
-                    text = Messanges.ANSWER.value.format(
-                        entity_id=f'{data.entity_id}'.split('-')[-1], text=transcript.text
-                    )
-                else:
-                    text = Messanges.EMPTY_TRANSCRIBE.value
+        async with message.process():
+            try:
+                data = LoadData.model_validate_json(message.body.decode())
+            except ValidationError:
+                logger.error(Messanges.VALIDATION_ERROR.value, message.body.decode())
+                return
+        if not os.access(data.audio_path, os.R_OK):
+            logger.error(Messanges.FILE_NOT_EXIST.value, data.audio_path)
+            return
+        with open(data.audio_path, mode='rb') as file:
+            transcript = self.openai_client.audio.transcriptions.create(
+                model=settings.openai_model, file=file
+            )
+        if transcript.text:
+            logger.info(
+                Messanges.TRANSCRIPT_SUCCESS.value, data.audio_path, len(transcript.text)
+            )
+            text = Messanges.ANSWER.value.format(
+                entity_id=f'{data.entity_id}'.split('-')[-1], text=transcript.text
+            )
+        else:
+            text = Messanges.EMPTY_TRANSCRIBE.value
 
-                messages = [
-                    text[i:i+settings.telegram_max_symbols_in_message]
-                    for i in range(0, len(text), settings.telegram_max_symbols_in_message)
-                ]
-                # logger.info(
-                #     Messanges.MESSAGE_DONE.value, message[:settings.logging_message_slice]
-                # )
-                await raw_sent_message_to_telegram(data.telegram_id, messages)
-            os.remove(data.audio_path)
-            logger.info(Messanges.AUDIO_DELETE.value, data.audio_path)
+        messages = [
+            text[i:i+settings.telegram_max_symbols_in_message]
+            for i in range(0, len(text), settings.telegram_max_symbols_in_message)
+        ]
+        # logger.info(
+        #     Messanges.MESSAGE_DONE.value, message[:settings.logging_message_slice]
+        # )
+        await raw_sent_message_to_telegram(data.telegram_id, messages)
+        os.remove(data.audio_path)
+        logger.info(Messanges.AUDIO_DELETE.value, data.audio_path)
 
     @backoff.on_exception(
         backoff.expo, ConnectionError, max_time=settings.backoff_max_time,
@@ -68,9 +67,12 @@ class Worker:
         try:
             channel = await connection.channel()
             await channel.set_qos(prefetch_count=count)
-            instant_queue = await channel.declare_queue(settings.transcribe_queue, durable=True)
+            instant_queue = await channel.declare_queue(
+                settings.transcribe_queue, durable=True, auto_delete=True
+            )
             while True:
-                await instant_queue.consume(callback=self.process_message)
+                async with self.lock:
+                    await instant_queue.consume(callback=self.process_message)
                 await sleep(settings.consume_timeout)
         finally:
             await connection.close()
